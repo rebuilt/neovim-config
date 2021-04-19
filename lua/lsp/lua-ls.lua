8acffb5acf5ed4e2757175fcb60ef0b7da7dfbf4
nelson <memoryman51@hotmail.com>
2021-04-19 15:16:12 +0200

    check for unix system first

--- lua/lsp/lua-ls.lua
+++ lua/lsp/lua-ls.lua
@@ -4,12 +4,14 @@ USER = vim.fn.expand('$USER')
 local sumneko_root_path = ""
 local sumneko_binary = ""
 
-if vim.fn.has("mac") == 1 then
-    sumneko_root_path = "/Users/" .. USER .. "/.config/nvim/lua-language-server"
-    sumneko_binary = "/Users/" .. USER .. "/.config/nvim/lua-language-server/bin/macOS/lua-language-server"
-elseif vim.fn.has("unix") == 1 then
+if vim.fn.has("unix") == 1 then
     sumneko_root_path = "/home/" .. USER .. "/.config/nvim/lua-language-server"
-    sumneko_binary = "/home/" .. USER .. "/.config/nvim/lua-language-server/bin/Linux/lua-language-server"
+    sumneko_binary = "/home/" .. USER ..
+                         "/.config/nvim/lua-language-server/bin/Linux/lua-language-server"
+elseif vim.fn.has("mac") == 1 then
+    sumneko_root_path = "/Users/" .. USER .. "/.config/nvim/lua-language-server"
+    sumneko_binary = "/Users/" .. USER ..
+                         "/.config/nvim/lua-language-server/bin/macOS/lua-language-server"
 else
     print("Unsupported system for sumneko")
 end
@@ -30,7 +32,10 @@ require'lspconfig'.sumneko_lua.setup {
             },
             workspace = {
                 -- Make the server aware of Neovim runtime files
-                library = {[vim.fn.expand('$VIMRUNTIME/lua')] = true, [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true}
+                library = {
+                    [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+                    [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true
+                }
             }
         }
     }
