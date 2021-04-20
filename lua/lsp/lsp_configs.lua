b86e08ca777c489ada05047687ac3dffa1b91e6e
nelson <memoryman51@hotmail.com>
2021-04-20 09:38:05 +0200

    changed buffer keybindings to global

--- lua/lsp/lsp_configs.lua
+++ lua/lsp/lsp_configs.lua
@@ -2,44 +2,69 @@ local nvim_lsp = require("lspconfig")
 
 local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
 local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+vim.api.nvim_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
 -- Mappings.
 local opts = {noremap = true, silent = true}
-buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-buf_set_keymap('n', '<space>wa',
-               '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-buf_set_keymap('n', '<space>wr',
-               '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-buf_set_keymap('n', '<space>wl',
-               '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',
-               opts)
-buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>',
-               opts)
-buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-buf_set_keymap('n', '<space>e',
-               '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-buf_set_keymap('n', '<leader>[', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>',
-               opts)
-buf_set_keymap('n', '<leader>]', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>',
-               opts)
-buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>',
-               opts)
+vim.api.nvim_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>',
+                        opts)
+vim.api
+    .nvim_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+vim.api.nvim_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+vim.api.nvim_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>',
+                        opts)
+vim.api.nvim_set_keymap('n', '<C-k>',
+                        '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<space>wa',
+                        '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<space>wr',
+                        '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>',
+                        opts)
+vim.api.nvim_set_keymap('n', '<space>wl',
+                        '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',
+                        opts)
+vim.api.nvim_set_keymap('n', '<space>D',
+                        '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>',
+                        opts)
+vim.api.nvim_set_keymap('n', '<space>ca',
+                        '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+vim.api
+    .nvim_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<space>e',
+                        '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>',
+                        opts)
+vim.api.nvim_set_keymap('n', '<leader>[',
+                        '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>]',
+                        '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<space>q',
+                        '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+
+-- symbols for autocomplete
+vim.lsp.protocol.CompletionItemKind = {
+    "   (Text) ", "   (Method)", "   (Function)",
+    "   (Constructor)", " ﴲ  (Field)", "[] (Variable)", "   (Class)",
+    " ﰮ  (Interface)", "   (Module)", " 襁 (Property)", "   (Unit)",
+    "   (Value)", " 練 (Enum)", "   (Keyword)", " ﬌  (Snippet)",
+    "   (Color)", "   (File)", "   (Reference)", "   (Folder)",
+    "   (EnumMember)", " ﲀ  (Constant)", " ﳤ  (Struct)", "   (Event)",
+    "   (Operator)", "   (TypeParameter)"
+}
 
 -- General language server
-nvim_lsp.efm.setup {}
+nvim_lsp.efm.setup {
+    cmd = {"/usr/bin/efm-langserver"},
+    filetypes = {"lua", "vim"}
+}
 
 -- Typescript language server
 nvim_lsp.tsserver.setup {}
 
 -- Ruby language server
 nvim_lsp.solargraph.setup {
+    -- filetypes = {"ruby", "rbs"},
+    -- cmd = {"/home/nelson/.rvm/gems/ruby-3.0.0/bin/solargraph", "stdio"},
     settings = {
         solargraph = {
             autoformat = true,
